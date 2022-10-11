be44926fb126c0bdebf0cfab92ee27891db7f7b7
Chijioke (Chukwu)  Peter <peterchijioke1@gmail.com>
2022-10-11 20:40:11 +0100

    update

---
+++ android/app/build/generated/rncli/src/main/java/com/facebook/react/PackageList.java
@@ -0,0 +1,94 @@
+
+package com.facebook.react;
+
+import android.app.Application;
+import android.content.Context;
+import android.content.res.Resources;
+
+import com.facebook.react.ReactPackage;
+import com.facebook.react.shell.MainPackageConfig;
+import com.facebook.react.shell.MainReactPackage;
+import java.util.Arrays;
+import java.util.ArrayList;
+
+// react-native-vector-icons
+import com.oblador.vectoricons.VectorIconsPackage;
+// @react-native-async-storage/async-storage
+import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
+// @react-native-firebase/app
+import io.invertase.firebase.app.ReactNativeFirebaseAppPackage;
+// @react-native-firebase/crashlytics
+import io.invertase.firebase.crashlytics.ReactNativeFirebaseCrashlyticsPackage;
+// @react-native-firebase/messaging
+import io.invertase.firebase.messaging.ReactNativeFirebaseMessagingPackage;
+// @react-native-firebase/perf
+import io.invertase.firebase.perf.ReactNativeFirebasePerfPackage;
+// @react-native-firebase/remote-config
+import io.invertase.firebase.config.ReactNativeFirebaseConfigPackage;
+// @react-native-google-signin/google-signin
+import com.reactnativegooglesignin.RNGoogleSigninPackage;
+// react-native-code-push
+import com.microsoft.codepush.react.CodePush;
+// react-native-safe-area-context
+import com.th3rdwave.safeareacontext.SafeAreaContextPackage;
+// react-native-screens
+import com.swmansion.rnscreens.RNScreensPackage;
+
+public class PackageList {
+  private Application application;
+  private ReactNativeHost reactNativeHost;
+  private MainPackageConfig mConfig;
+
+  public PackageList(ReactNativeHost reactNativeHost) {
+    this(reactNativeHost, null);
+  }
+
+  public PackageList(Application application) {
+    this(application, null);
+  }
+
+  public PackageList(ReactNativeHost reactNativeHost, MainPackageConfig config) {
+    this.reactNativeHost = reactNativeHost;
+    mConfig = config;
+  }
+
+  public PackageList(Application application, MainPackageConfig config) {
+    this.reactNativeHost = null;
+    this.application = application;
+    mConfig = config;
+  }
+
+  private ReactNativeHost getReactNativeHost() {
+    return this.reactNativeHost;
+  }
+
+  private Resources getResources() {
+    return this.getApplication().getResources();
+  }
+
+  private Application getApplication() {
+    if (this.reactNativeHost == null) return this.application;
+    return this.reactNativeHost.getApplication();
+  }
+
+  private Context getApplicationContext() {
+    return this.getApplication().getApplicationContext();
+  }
+
+  public ArrayList<ReactPackage> getPackages() {
+    return new ArrayList<>(Arrays.<ReactPackage>asList(
+      new MainReactPackage(mConfig),
+      new VectorIconsPackage(),
+      new AsyncStoragePackage(),
+      new ReactNativeFirebaseAppPackage(),
+      new ReactNativeFirebaseCrashlyticsPackage(),
+      new ReactNativeFirebaseMessagingPackage(),
+      new ReactNativeFirebasePerfPackage(),
+      new ReactNativeFirebaseConfigPackage(),
+      new RNGoogleSigninPackage(),
+      new CodePush(getResources().getString(com.fp_news.R.string.CodePushDeploymentKey), getApplicationContext(), com.fp_news.BuildConfig.DEBUG),
+      new SafeAreaContextPackage(),
+      new RNScreensPackage()
+    ));
+  }
+}
