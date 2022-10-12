1459bc88d9328cf0bf9bf724f0e27a536050a8e1
Chijioke (Chukwu)  Peter <peterchijioke1@gmail.com>
2022-10-12 14:20:42 +0100

    update

--- src/infrastructure/services/api/news/Index.ts
+++ src/infrastructure/services/api/news/Index.ts
@@ -1,13 +1,13 @@
 import crashlytics from "@react-native-firebase/crashlytics";
 import  Config from "../../../../../Config";
 
- const url = 'https://newscatcher.p.rapidapi.com/v1/search_free?'
+ const url = Config.base_url+'search_free?'
    const GetNewsApi= async () => {
                               let params = new URLSearchParams({q: 'Elon Musk', lang: 'en', media: 'True'})
                              try {
                                const response = await fetch(url+ params,{
                                    headers: {
-                                  'X-RapidAPI-Key': `36fd4b0648mshe6ff39a9fff795ep19ff77jsna772ea0effce`,
+                                  'X-RapidAPI-Key': `${Config.api_key}`,
                                   'X-RapidAPI-Host': 'newscatcher.p.rapidapi.com' }});
                                 return response
                              } catch (error:any) {
