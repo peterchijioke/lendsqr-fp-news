597a7234c613a94a8a8ff27045837a52accacb01
Chijioke (Chukwu)  Peter <peterchijioke1@gmail.com>
2022-10-12 10:16:04 +0100

    update

--- src/screens/NewsListing.tsx
+++ src/screens/NewsListing.tsx
@@ -2,15 +2,15 @@ import { SafeAreaView, StyleSheet, Text, View } from "react-native";
 import React from "react";
 import { Colors } from "../assets/colors/Colors";
 import Wrapper from "./components/wrapper/Wrapper";
+import { elevation } from "../partials/Style";
+import AppText from "./components/text/AppText";
 export let newsListingName = "newsListing";
 const NewsListing = () => {
   return (
     <SafeAreaView style={styles.container}>
-      <Wrapper style={styles.cardWrapper}>
-        <Wrapper.Card
-          style={{ height: "30%", width: "90%", borderRadius: 5 }}
-        ></Wrapper.Card>
-      </Wrapper>
+      <Wrapper.Card style={{ elevation: 4, width: "95%" }}>
+        <AppText>peter</AppText>
+      </Wrapper.Card>
     </SafeAreaView>
   );
 };
@@ -18,11 +18,10 @@ const NewsListing = () => {
 export default NewsListing;
 
 const styles = StyleSheet.create({
-  container: { display: "flex", flex: 1, backgroundColor: Colors.secondary },
-  cardWrapper: {
-    height: "100%",
-    width: "100%",
-    paddingLeft: "10%",
-    paddingRight: "10%",
+  container: {
+    display: "flex",
+    flex: 1,
+    backgroundColor: Colors.secondary,
+    alignItems: "center",
   },
 });
--- src/screens/components/wrapper/Wrapper.tsx
+++ src/screens/components/wrapper/Wrapper.tsx
@@ -1,13 +1,15 @@
-import {StyleSheet, Text, View, ViewProps} from 'react-native';
-import React from 'react';
-import {elevation} from '../../../partials/Style';
-import {Colors} from '../../../assets/colors/Colors';
+import { StyleSheet, Text, View, ViewProps } from "react-native";
+import React from "react";
+import { elevation } from "../../../partials/Style";
+import { Colors } from "../../../assets/colors/Colors";
 
-const Wrapper = ({style, ...props}: ViewProps) => {
+const Wrapper = ({ style, ...props }: ViewProps) => {
   return <View style={[styles.container, style]} {...props} />;
 };
 
-Wrapper.Card = (props: ViewProps) => <Wrapper style={[styles.card]} />;
+Wrapper.Card = (props: ViewProps) => (
+  <Wrapper style={[styles.card]} {...props} />
+);
 export default Wrapper;
 
 const styles = StyleSheet.create({
@@ -15,8 +17,7 @@ const styles = StyleSheet.create({
     backgroundColor: Colors.secondary,
   },
   card: {
-    ...elevation,
-    width: '95%',
-    borderRadius: 10,
+    width: "100%",
+    backgroundColor: Colors.primary,
   },
 });
