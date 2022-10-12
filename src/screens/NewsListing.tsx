ad44f28a9e59bb80986c07f9babea0003c4957c2
Chijioke (Chukwu)  Peter <peterchijioke1@gmail.com>
2022-10-12 12:17:40 +0100

    update

--- src/screens/NewsListing.tsx
+++ src/screens/NewsListing.tsx
@@ -1,4 +1,12 @@
-import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import {
+  FlatList,
+  Image,
+  ListRenderItemInfo,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 import React, { useEffect } from "react";
 import { Colors } from "../assets/colors/Colors";
 import Wrapper from "./components/wrapper/Wrapper";
@@ -19,23 +27,50 @@ const NewsListing = () => {
   const loading = useSelector(getNewsLoading);
 
   useEffect(() => {
-    console.log("===============================================");
-    console.log(allNews?.articles?.length);
-    console.log("===============================================");
-  }, [allNews]);
-
-  useEffect(() => {
     dispatch(NEWS_LOAD(true));
   }, [dispatch]);
 
+  const ItemComponent = ({ item, index }): any => {
+    if (index == 1) {
+      return (
+        <Wrapper.Card style={{ elevation: 5 }}>
+          <View style={{ flexDirection: "row", padding: 5 }}>
+            <Wrapper
+              style={{ height: 20, width: 20, backgroundColor: "black" }}
+            ></Wrapper>
+            <AppText.Title style={{ marginLeft: 5 }}>
+              {item.topic.toUpperCase()}
+            </AppText.Title>
+          </View>
+          <Wrapper style={{ width: "100%", height: 200, padding: 5 }}>
+            <Image
+              style={{ width: "100%", height: "100%", borderRadius: 5 }}
+              source={{ uri: `${item?.media}` }}
+            ></Image>
+          </Wrapper>
+          <AppText.SubTitle style={{ marginLeft: 5 }}>
+            {item.title}
+          </AppText.SubTitle>
+          <Wrapper style={{ marginTop: 5 }}>
+            <AppText.Body>{item?.published_date}</AppText.Body>
+          </Wrapper>
+        </Wrapper.Card>
+      );
+    }
+  };
+
   return (
     <SafeAreaView style={styles.container}>
-      <Wrapper.Card style={{ elevation: 4 }}>
-        <View
-          style={{ height: 20, width: 20, backgroundColor: "black" }}
-        ></View>
-        <AppText.SubTitle>peter</AppText.SubTitle>
-      </Wrapper.Card>
+      <View style={{ height: "100%", width: "100%" }}>
+        <FlatList
+          style={{ flex: 1 }}
+          keyExtractor={(item, index) => `${index}`}
+          data={allNews?.articles}
+          renderItem={({ item, index }: any) => (
+            <ItemComponent item={item} index={index} />
+          )}
+        />
+      </View>
     </SafeAreaView>
   );
 };
