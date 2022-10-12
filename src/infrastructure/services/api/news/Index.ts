0f621f19323200d5303b0dfddd51d13372b3a446
Chijioke (Chukwu)  Peter <peterchijioke1@gmail.com>
2022-10-12 11:00:53 +0100

    update

--- src/infrastructure/services/api/news/Index.ts
+++ src/infrastructure/services/api/news/Index.ts
@@ -3,16 +3,15 @@ import  Config from "../../../../../Config";
  const url = 'https://newscatcher.p.rapidapi.com/v1/search_free?'
    const GetNewsApi= async () => {
                               let params = new URLSearchParams({q: 'Elon Musk', lang: 'en', media: 'True'})
-                             try {
-                               const response = await fetch(url+ params,{
+                          
+                               fetch(url+ params,{
                                    headers: {
                                   'X-RapidAPI-Key': `36fd4b0648mshe6ff39a9fff795ep19ff77jsna772ea0effce`,
-                                  'X-RapidAPI-Host': 'newscatcher.p.rapidapi.com' }});
-                                  console.log(response.json())
-                             } catch (error) {
-                              console.log(error)
-                             }   
-                                  // return response
+                                  'X-RapidAPI-Host': 'newscatcher.p.rapidapi.com' }}).then(res=>res.json()).then((response)=>{
+                                        console.log(response)
+                                  }).catch(e=>console.log("=============",e))
+                                  
+                              // return response
              }
 
 
